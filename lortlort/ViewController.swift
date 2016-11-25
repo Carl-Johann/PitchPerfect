902fa3cfabf95bedc486c71d230835ea553dca5d
Carl-Johan Beurling <carljohan.beurling@gmail.com>
2016-11-25 16:57:20 +0100

    Initial Commit

---
+++ lortlort/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lortlort
+//
+//  Created by CarlJohan on 25/11/16.
+//  Copyright © 2016 Carl-Johan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
